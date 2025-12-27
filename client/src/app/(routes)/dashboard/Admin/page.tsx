cc4ab56fb28bd841a8c92218296f0dc28e963cee
Adityakbr01 <aditykbr01@gmail.com>
2025-12-27 16:25:07 +0530

    fix: Update Next.js configuration and improve path resolution in TypeScript

--- client/src/app/(routes)/dashboard/Admin/page.tsx
+++ client/src/app/(routes)/dashboard/Admin/page.tsx
@@ -1,7 +1,7 @@
 import DashBoardPage from "@/components/pages/dashboard/admin/DashBoardPage";
 import RoleGate from "@/components/pages/dashboard/RoleGate";
 import APP_info from "@/lib/constants/app_info";;
-import { ROLES } from "@/validators/auth.schema";
+import { ROLES } from "@/lib/constants/roles";
 
 export const metadata = {
     title: `Admin Dashboard | ${APP_info.NAME}`,
