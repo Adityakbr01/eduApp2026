cd24c9e753f7cc61f68439b2d0e88ddc02768547
Adityakbr01 <aditykbr01@gmail.com>
2026-01-09 12:45:15 +0530

    refactor: Update .dockerignore files and remove obsolete deployment scripts

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,18 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  productionBrowserSourceMaps: false,
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "eduapp2026-s3-bucket.s3.us-east-1.amazonaws.com",
-      },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "eduapp2026-s3-bucket.s3.us-east-1.amazonaws.com" },
     ],
   },
 };
+
 
 export default nextConfig;
